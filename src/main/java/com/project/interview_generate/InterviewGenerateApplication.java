f6652345fcc1ade5c96177bd4780e2b1af15f3bb
dlxortmd987 <dlxortmd987@gmail.com>
2023-04-29 22:24:14 +0900

    init setting

---
+++ src/main/java/com/project/interview_generate/InterviewGenerateApplication.java
@@ -0,0 +1,13 @@
+package com.project.interview_generate;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InterviewGenerateApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(InterviewGenerateApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/project/interview_generate/InterviewGenerateApplicationTests.java
@@ -0,0 +1,13 @@
+package com.project.interview_generate;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InterviewGenerateApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
